f1dea09fd0d2cf1e198511755d45da80cb9f126f
KondalaRaoManoj <gutlapalli999@gmail.com>
2025-06-25 00:22:55 +0200

    Added Documentation, Test evidences

--- TestCase.sh
+++ TestCase.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-docker start ar_project_2_udp_sender_run_7c27a26d14c8 > /dev/null 2>&1
+docker start udp_sender > /dev/null 2>&1
 sleep 1 # a small delay to initialize the container
-docker exec -i ar_project_2_udp_sender_run_7c27a26d14c8 ./ar_udp_sender <<EOF
+docker exec -i udp_sender ./ar_udp_sender <<EOF
 1
 Hello
 2
@@ -12,7 +12,7 @@ Automated
 4
 UDP
 5
-KondalaRao Manoj
+KondalaRao Manoj3
 6
 Gutlapalli
 STOP
